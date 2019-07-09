ee589c91fd2a7a404000f0cd4b682271c1ad8069
Anastasiia Barabash <nastya.barabash@gmail.com>
2019-07-09 23:53:22 +0300

    Small fix.

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -4,10 +4,9 @@ const MAX_RANGE = 8;
 let n = parseInt(Math.random()*MAX_RANGE);
 
 if (confirm('Do you want to play a game?')) {
-    alert(n);
     let pocket = prompt('Choose a roulette pocket number from 0 to 8 \nAttempts left: 3', number);
    if (pocket === n){
-       alert('true1')
+       alert('true');
    }
 } else {
     alert('You did not become a billionaire, but can.');
